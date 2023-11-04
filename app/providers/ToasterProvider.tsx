830cf6a195f5db402ffe17657f498c503e70fa6d
ibra-kdbra <git-repo-develop@outlook.com>
2023-11-05 00:03:15 +0300

    hooks and provides with the prisma schema

---
+++ app/providers/ToasterProvider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { Toaster } from "react-hot-toast"
+
+type Props = {}
+
+const ToasterProvider = (props: Props) => {
+  return (
+    <Toaster
+    
+    />
+  );
+}
+
+export default ToasterProvider;
