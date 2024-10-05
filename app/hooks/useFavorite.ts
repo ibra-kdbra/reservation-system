ab12fe37791b41a0927df195036684f7d8623e74
ibra-kdbra <git-repo-develop@outlook.com>
2024-10-05 17:17:34 +0300

    all primary hooks updated format

--- app/hooks/useFavorite.ts
+++ app/hooks/useFavorite.ts
@@ -1,63 +1,54 @@
-import axios from "axios";
-import { useRouter } from "next/navigation";
-import { useCallback, useMemo } from "react";
-import { toast } from "react-hot-toast";
+import axios from "axios"
+import { useRouter } from "next/navigation"
+import {toast} from "react-hot-toast"
 
-import { SafeUser } from "@/app/types";
+import { SafeUser } from "../types"
+import useLoginModal from "./useLoginModal"
+import { useCallback, useMemo } from "react"
 
-import useLoginModal from "./useLoginModal";
-
-interface IUseFavorite {
-  listingId: string;
+interface IUseFavorite { 
+  listingId: string
   currentUser?: SafeUser | null
 }
 
-const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
-  const router = useRouter();
-
-  const loginModal = useLoginModal();
+const useFavorite = ({listingId, currentUser}: IUseFavorite) => {
+  const router = useRouter()
+  const loginModal = useLoginModal()
 
   const hasFavorited = useMemo(() => {
-    const list = currentUser?.favoriteIds || [];
-
-    return list.includes(listingId);
-  }, [currentUser, listingId]);
+    const list = currentUser?.favoriteIds || []
+    return list.includes(listingId)
+  }, [currentUser, listingId])
 
   const toggleFavorite = useCallback(async (e: React.MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation();
-
-    if (!currentUser) {
-      return loginModal.onOpen();
-    }
+    e.stopPropagation()
+    if(!currentUser) return loginModal.onOpen()
 
     try {
-      let request;
+      let request
 
-      if (hasFavorited) {
-        request = () => axios.delete(`/api/favorites/${listingId}`);
-      } else {
-        request = () => axios.post(`/api/favorites/${listingId}`);
+      if(hasFavorited){
+        request = () => axios.delete(`/api/favorites/${listingId}`)
+      }else{
+        request = () => axios.post(`/api/favorites/${listingId}`)
       }
 
-      await request();
-      router.refresh();
-      toast.success('Success');
+      await request()
+      router.refresh()
+      toast.success("Success")
+      
     } catch (error) {
-      toast.error('Something went wrong.');
+      toast.error("Something went wrong")
     }
-  }, 
-  [
-    currentUser, 
-    hasFavorited, 
-    listingId, 
+  }, [
+    currentUser,
+    hasFavorited,
+    listingId,
     loginModal,
     router
-  ]);
+  ])
 
-  return {
-    hasFavorited,
-    toggleFavorite,
-  }
+  return {hasFavorited, toggleFavorite}
 }
 
-export default useFavorite;
+export default useFavorite
