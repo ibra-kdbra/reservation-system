a7594a12145a6409424fdbc16db06023fc382135
ibra-kdbra <git-repo-develop@outlook.com>
2024-10-05 17:18:20 +0300

    providers, client reserve

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -1,78 +1,47 @@
-'use client';
+"use client"
 
-import { toast } from "react-hot-toast";
-import axios from "axios";
-import { useCallback, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation"
+import Container from "../components/Container"
+import { SafeReservation, SafeUser } from "../types"
+import Heading from "../components/Heading"
+import ListingCard from "../components/listings/ListingCard"
+import { useCallback, useState } from "react"
+import toast from "react-hot-toast"
+import axios from "axios"
 
-import { SafeReservation, SafeUser } from "@/app/types"
-;
-import Heading from "@/app/components/Heading";
-import Container from "@/app/components/Container";
-import ListingCard from "@/app/components/listings/ListingCard";
-
-interface ReservationsClientProps {
-  reservations: SafeReservation[],
-  currentUser?: SafeUser | null,
+interface ReservationsClient{
+  reservations: SafeReservation[]
+  currentUser: SafeUser
 }
 
-const ReservationsClient: React.FC<ReservationsClientProps> = ({
-  reservations,
-  currentUser
-}) => {
-  const router = useRouter();
-  const [deletingId, setDeletingId] = useState('');
+const ReservationsClient = ({reservations, currentUser}: ReservationsClient) => {
+  const router = useRouter()
+
+  const [deletingId, setDeletingId] = useState("")
 
   const onCancel = useCallback((id: string) => {
-    setDeletingId(id);
+    setDeletingId(id)
 
-    axios.delete(`/api/reservations/${id}`)
-    .then(() => {
-      toast.success('Reservation cancelled');
-      router.refresh();
+    axios.delete(`/api/reservations/${id}`).then(() => {
+      toast.success("Reservation cancelled")
+      router.refresh()
+    }).catch((error) => {
+      toast.error("Something went wrong")
+    }).finally(() => {
+      setDeletingId("")
     })
-    .catch(() => {
-      toast.error('Something went wrong.')
-    })
-    .finally(() => {
-      setDeletingId('');
-    })
-  }, [router]);
+  }, [router])
 
   return (
     <Container>
-      <Heading
-        title="Reservations"
-        subtitle="Bookings on your properties"
-      />
-      <div 
-        className="
-          mt-10
-          grid 
-          grid-cols-1 
-          sm:grid-cols-2 
-          md:grid-cols-3 
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
-      >
-        {reservations.map((reservation: any) => (
-          <ListingCard
-            key={reservation.id}
-            data={reservation.listing}
-            reservation={reservation}
-            actionId={reservation.id}
-            onAction={onCancel}
-            disabled={deletingId === reservation.id}
-            actionLabel="Cancel guest reservation"
-            currentUser={currentUser}
-          />
-        ))}
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4">
+      {reservations.map((reservation) => (
+        <ListingCard key={reservation.id} data={reservation.listing} reservation={reservation} actionId={reservation.id} onAction={onCancel} disabled={deletingId === reservation.id} actionLabel="Cancel reservation" currentUser={currentUser} />
+      ))}
       </div>
     </Container>
-   );
+  )
 }
- 
-export default ReservationsClient;
+
+export default ReservationsClient
--- app/reservations/page.tsx
+++ app/reservations/page.tsx
@@ -1,47 +1,28 @@
+import EmptyState from "../components/EmptyState";
 
-import EmptyState from "@/app/components/EmptyState";
-import ClientOnly from "@/app/components/ClientOnly";
-
-import getCurrentUser from "@/app/actions/getCurrentUser";
-import getReservations from "@/app/actions/getReservations";
-
-import TripsClient from "./ReservationsClient";
+import getCurrentUser from "../actions/GetCurrentUser";
+import getReservations from "../actions/GetReservations";
+import ReservationsClient from "./ReservationsClient";
 
 const ReservationsPage = async () => {
-  const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser()
 
-  if (!currentUser) {
+  if(!currentUser) return <EmptyState title="Unauthorized" subtitle="Please login" />
+
+  const reservations = await getReservations({
+    authorId: currentUser.id
+  })
+
+  if(reservations.length === 0 ){
     return (
-      <ClientOnly> 
-        <EmptyState
-          title="Unauthorized"
-          subtitle="Please login"
-        />
-      </ClientOnly>
+      <EmptyState title="No reservations found" subtitle="Looks like you have no reservations on your properties" />
     )
   }
 
-  const reservations = await getReservations({ authorId: currentUser.id });
+  return(
+    <ReservationsClient reservations={reservations} currentUser={currentUser} />
+  )
 
-  if (reservations.length === 0) {
-    return (
-      <ClientOnly>
-        <EmptyState
-          title="No reservations found"
-          subtitle="Looks like you have no reservations on your properties."
-        />
-      </ClientOnly>
-    );
-  }
-
-  return (
-    <ClientOnly>
-      <TripsClient
-        reservations={reservations}
-        currentUser={currentUser}
-      />
-    </ClientOnly>
-  );
 }
 
-export default ReservationsPage;
+export default ReservationsPage
