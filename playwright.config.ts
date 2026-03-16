482aed269a2a6f7ba3152aa64efb2f2f5d036b18
ibra-kdbra <git-repo-develop@outlook.com>
2026-03-16 23:44:14 +0300

    test(e2e): update playwright config for CI/CD synchronization

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,8 +46,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:3000/api/docs',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
+    timeout: 180 * 1000,
   },
 });
